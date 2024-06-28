5a3de89d5e5b5b9c7e3695fa46baef3f91104bd3
Leah Deshar <leah.desar@gmail.com>
2024-06-28 21:41:59 +0545

    design UI

--- client/components/HousePricePredict.jsx
+++ client/components/HousePricePredict.jsx
@@ -9,6 +9,7 @@ import {
   Alert,
 } from "react-native";
 import axios from "axios";
+
 const formFields = [
   { name: "bedrooms", placeholder: "Number of Bedrooms" },
   { name: "bathrooms", placeholder: "Number of Bathrooms" },
@@ -24,9 +25,9 @@ const formFields = [
   { name: "yr_renovated", placeholder: "Year Renovated" },
 ];
 
-const HousePricePredict = () => {
+export default function HousePricePredict() {
   const [form, setForm] = useState(
-    formFields.reduce((acc, field) => ({ ...acc, [field]: "" }), {})
+    formFields.reduce((acc, field) => ({ ...acc, [field.name]: "" }), {})
   );
   const [predictedPrice, setPredictedPrice] = useState(null);
 
@@ -34,19 +35,46 @@ const HousePricePredict = () => {
     setForm({ ...form, [name]: value });
   };
 
+  const handleTest = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/test/");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const handleSubmit = async () => {
     try {
       console.log(form);
+      const formData = {
+        bedrooms: parseInt(form.bedrooms),
+        bathrooms: parseFloat(form.bathrooms),
+        sqft_living: parseInt(form.sqft_living),
+        sqft_lot: parseInt(form.sqft_lot),
+        floors: parseFloat(form.floors),
+        waterfront: parseInt(form.waterfront),
+        view: parseInt(form.view),
+        condition: parseInt(form.condition),
+        sqft_above: parseInt(form.sqft_above),
+        sqft_basement: parseInt(form.sqft_basement),
+        yr_built: parseInt(form.yr_built),
+        yr_renovated: parseInt(form.yr_renovated),
+      };
       const response = await axios.post(
         "http://127.0.0.1:8000/predict-house-price/",
-        form
+        formData
       );
       setPredictedPrice(response.data.predicted_price);
     } catch (error) {
+      if (error.response) {
+        console.error("Response data:", error.response.data);
+      }
       console.error(error);
-      Alert.alert("Error", "There was an error making the prediction");
+      Alert.alert("Error", `There was an error making the prediction ${error}`);
     }
   };
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>House Price Predictor</Text>
@@ -56,6 +84,7 @@ const HousePricePredict = () => {
           <TextInput
             key={field.name}
             style={styles.input}
+            placeholder={field.placeholder}
             value={form[field.name]}
             onChangeText={(value) => handleChange(field.name, value)}
             keyboardType="numeric"
@@ -63,6 +92,7 @@ const HousePricePredict = () => {
         </View>
       ))}
       <Button title="Predict Price" onPress={handleSubmit} />
+      <Button title="Test" onPress={handleTest} />
       {predictedPrice !== null && (
         <Text style={styles.result}>
           Predicted Price: ${predictedPrice.toFixed(2)}
@@ -70,9 +100,8 @@ const HousePricePredict = () => {
       )}
     </ScrollView>
   );
-};
+}
 
-export default HousePricePredict;
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
